630204ac99cd3a21c9d71bb14253928065bda491
nadirdere <mehmet.nadir.dere@hicoders.ch>
2022-12-05 17:06:20 +0100

    "first commit"

--- functions.js
+++ functions.js
@@ -1,10 +1,21 @@
-const getProductsOfNormalQuality = () => {};
+import { productList } from "./data.js";
 
-const getProductImageWithPngExtension = () => {};
+const getProductsOfNormalQuality = (pProducts) => {
+  return pProducts.filter((product) => product.quality == "Normal");
+};
 
-const getCalorieOfTheMostExpensiveProduct = () => {};
+const getProductImageWithPngExtension = (pProducts) => {
+  return pProducts.filter((product) => product.productImage.endsWith(".png"));
+};
 
-const sortByExpirationDate = () => {};
+const getCalorieOfTheMostExpensiveProduct = (pProducts) => {
+  const sortedProducts = pProducts.sort((product1, product2) => product2.price - product1.price);
+  return sortedProducts[0].totalCalories;
+};
+
+const sortByExpirationDate = (pProducts) => {
+  return pProducts.sort((product1, product2) => product1.expireDate - product2.expireDate)
+};
 
 export {
   getProductsOfNormalQuality,
